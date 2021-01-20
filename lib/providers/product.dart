57c3c25d3926f65f79302af21d65360d172146b5
emtalen <59577366+emtalen@users.noreply.github.com>
2021-01-20 10:29:57 +0100

    adding functionality to update the favorites in the server

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +19,30 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  void toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    final url =
+        'https://flutterlearning-9fe4d-default-rtdb.europe-west1.firebasedatabase.app/products/$id.json';
+    try {
+      final response = await http.put(
+        url,
+        body: json.encode({
+          'isFavorite': isFavorite,
+        }),
+      );
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (error) {
+      isFavorite = oldStatus;
+      _setFavValue(oldStatus);
+    }
   }
 }
