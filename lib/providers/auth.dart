1a220e743cbaa163bdd0dc91cf0aab0b0331d968
emtalen <59577366+emtalen@users.noreply.github.com>
2021-01-29 16:53:13 +0100

    adding errorhandling for the login functionality

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -2,22 +2,40 @@ import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import '../models/http_exception.dart';
+
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signUp(String email, String password) async {
+  Future<void> _authenticate(
+      String email, String password, String endPoint) async {
     final url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDnn2Ry7HMkF5o_W6tRWAGcbfOPsKXnvDg';
-    final response = await http.post(
-      url,
-      body: json.encode({
-        'email': email,
-        'password': password,
-        'returnSecureToken': true,
-      }),
-    );
-    print(json.decode(response.body));
+        'https://identitytoolkit.googleapis.com/v1/accounts:$endPoint?key=AIzaSyDnn2Ry7HMkF5o_W6tRWAGcbfOPsKXnvDg';
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode({
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        }),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  Future<void> signUp(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> logIn(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
